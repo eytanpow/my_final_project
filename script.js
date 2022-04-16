3b97f05215115d89ddc53fb315dc5874a4336473
eytanpow <cgmeytanperos@gmail.com>
2022-04-16 12:12:13 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
 }
-        
+function updateRate() 
+{
+    var x = document.getElementById("Interest").value;
+    document.getElementById("rate_val").innerHTML=x+"%";
+}    
+
+function updatecheck() 
+{
+    var rateval = "checked";
+    document.getElementById("check").innerText=rateval;
+}    
