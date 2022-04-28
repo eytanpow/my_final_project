021843230017546dad25d39b84cd73e0a4c6426c
eytanpow <cgmeytanperos@gmail.com>
2022-04-28 13:21:28 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
 function compute()
 {
+    if(validateForm())
+    {
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
     /*document.getElementById("result").innerHTML="";*/
-    var deposit="If you deposit "+principal+",\<br\>"
-    var rate="at an interest rate of "+rate+"\<br\>"
-    var recieve="You will receive an amount of "+interest+"\<br\>";
-    var year="in the year "+year
+    var deposit="If you deposit <mark>"+principal+"</mark>,\<br\>"
+    var rate="at an interest rate of <mark>"+rate+"</mark>\<br\>"
+    var recieve="You will receive an amount of <mark>"+interest+"</mark>\<br\>";
+    var year="in the year <mark>"+year+"</mark>"
     document.getElementById("result").innerHTML=deposit+rate+recieve+year;
+    }
 }
 function updateRate() 
 {
     var x = document.getElementById("rate").value;
     document.getElementById("rate_val").innerHTML=x+"%";
-}    
+}  
+function validateForm()
+ {
+    var principal = document.getElementById("principal").value;
+    if (principal <=0) {
+      alert("principal must be bigger than 0");
+      document.getElementById("principal").focus();
+      return false;
+    }
+    else
+    return true;
+  }  
 
