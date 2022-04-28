a8639ef90cbe0ea0aafa9e9693a14121d4e9514c
eytanpow <cgmeytanperos@gmail.com>
2022-04-28 11:50:13 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -2,10 +2,15 @@ function compute()
 {
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
-    document.getElementById("result").innerHTML="";
-    document.getElementById("result").innerHTML=principal+interest;
+    /*document.getElementById("result").innerHTML="";*/
+    var deposit="If you deposit "+principal+",\<br\>"
+    var rate="at an interest rate of "+rate+"\<br\>"
+    var recieve="You will receive an amount of "+interest+"\<br\>";
+    var year="in the year "+year
+    document.getElementById("result").innerHTML=deposit+rate+recieve+year;
 }
 function updateRate() 
 {
